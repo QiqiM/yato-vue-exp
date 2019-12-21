919a5832f658c6fd94ac99927e798761f576233b
yato <2602231824@qq.com>
2019-12-21 22:39:39 +0800

    添加数据库模型

---
+++ node-server/dao/models/button.js
@@ -0,0 +1,25 @@
+const gmLogger = require('../../utils/logger')('gm');
+const appLogger = require('../../utils/logger')('app');
+const mongoose = require('mongoose');
+const mongodb = require('../mongodb');
+const Schema = mongoose.Schema;
+
+// 创建Schema
+const buttonSchema = new Schema({
+    code: { type: String, required: true },   // unique 以此字段为唯一索引
+    name: { type: String, required: true },
+    desc: { type: String },
+    state: { type: Boolean },
+    timeStamp: { type: Date, default: Date.now }
+}, { collation: "user", versionKey: false, bufferCommands: false, usePushEach: true });
+
+let _model;
+let getModel = function () {
+    if (!_model)
+        _model = mongodb.getMongodbConn("business").model('button', buttonSchema);
+
+    return _model;
+}
+
+module.exports.getModel = getModel;
+
